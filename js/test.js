489e12a48867067e084918b46a58c82044bc7e3f
LingZhe <1119538589@qq.com>
2023-07-19 19:16:04 +0900

    Site updated: 2023-07-19 19:16:04

--- js/test.js
+++
@@ -1,6 +0,0 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const randomFunction = generateRandomFunction();
-console.log(randomFunction);
